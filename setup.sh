cf93a1a634aa24cda500a0fb261de7513ca36ca1
Alex <alexander.strassheim@me.com>
2021-12-09 17:03:52 +0100

    update setup file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-DOTDIR=${HOME}/dotfilesTWO
+DOTDIR=${HOME}/dotfiles
 ALACRITYDIR=${HOME}/.config/alacritty
 NVIMDIR=${HOME}/.config/nvim
 
 if [ ! -d ${DOTDIR} ]; then
+  echo "Creating and cloning dot project..."
   mkdir ${DOTDIR}
   git clone git@github.com:alexstrassheim/dotfiles.git ${DOTDIR}
 else
+  echo "Updating dot files..."
   cd ${DOTDIR}
   git pull
 fi
